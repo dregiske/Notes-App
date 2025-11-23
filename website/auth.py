1c7bc52ade6a8fa893e1d09a9cfe3c032966a979
Andre Giske <agiske@ucsd.edu>
2025-11-22 20:32:35 -0800

    working notes feature

--- website/auth.py
+++ website/auth.py
@@ -44,7 +44,7 @@ def signup():
 				login_user(new_user, remember=True)
 				return redirect(url_for('views.home'))
 
-	return render_template("signup.html")
+	return render_template("signup.html", user=current_user)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -63,7 +63,7 @@ def login():
 		else:
 			flash('Email does not exist.', category='error')
 
-	return render_template("login.html")
+	return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
