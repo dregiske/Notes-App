5f0a346ec81db5f760852e3c249e5f2cfdc53f55
Andre Giske <agiske@ucsd.edu>
2025-11-22 20:56:39 -0800

    delete note feature

--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 
 from flask_login import login_required, current_user
 
 from .models import Note
 
 from . import db
+
+import json
+
 
 views = Blueprint('views', __name__)
 
@@ -23,3 +26,17 @@ def home():
 			flash('Note added.', category='success')
 
 	return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+@login_required
+def delete_note():
+	note = json.loads(request.data)
+	noteId = note['noteId']
+	note = Note.query.get(noteId)
+
+	if note:
+		if note.user_id == current_user.id:
+			db.session.delete(note)
+			db.session.commit()
+	
+	return jsonify({})
