bb229b9f289565b4c1c275ba63043f697217876d
Andre Giske <agiske@ucsd.edu>
2025-11-22 16:06:39 -0800

    flask messages

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+	if request.method == 'POST':
+		email = request.form.get('email')
+		firstName = request.form.get('firstName')
+		password1 = request.form.get('password1')
+		password2 = request.form.get('password2')
+
+		if len(email) < 4:
+			flash('Email must be greater than 4 characters.', category='error')
+		if len(firstName) < 2:
+			flash('First name must be greater than 1 character.', category='error')
+		if password1 != password2:
+			flash('Passwords don\'t match.', category='error')
+		if len(password1) < 7:
+			flash('Password must be at least 7 characters long.', category='error')
+		else:
+			flash('Accounted created.', category='success')
+
 	return render_template("signup.html")
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template("login.html", text="String")
+	return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
