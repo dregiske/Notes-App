7837b5925bf2551b78686ea027aa2d63b0446ee5
Andre Giske <agiske@ucsd.edu>
2025-11-22 18:23:29 -0800

    working signup / login / logout

--- website/auth.py
+++ website/auth.py
@@ -22,27 +22,27 @@ def signup():
 		user = User.query.filter_by(email=email).first()
 		if user:
 			flash('Email already exists.', category='error')
-
-		if len(email) < 4:
-			flash('Email must be greater than 4 characters.', category='error')
-		if len(firstName) < 2:
-			flash('First name must be greater than 1 character.', category='error')
-		if password1 != password2:
-			flash('Passwords don\'t match.', category='error')
-		if len(password1) < 7:
-			flash('Password must be at least 7 characters long.', category='error')
 		else:
-			hashed_password = generate_password_hash(password1, method='scrypt')
-			new_user = User(email=email,
-							password=hashed_password,
-							first_name=firstName,
-			)
-			db.session.add(new_user)
-			db.session.commit()
+			if len(email) < 4:
+				flash('Email must be greater than 4 characters.', category='error')
+			if len(firstName) < 2:
+				flash('First name must be greater than 1 character.', category='error')
+			if password1 != password2:
+				flash('Passwords don\'t match.', category='error')
+			if len(password1) < 7:
+				flash('Password must be at least 7 characters long.', category='error')
+			else:
+				hashed_password = generate_password_hash(password1, method='scrypt')
+				new_user = User(email=email,
+								password=hashed_password,
+								first_name=firstName,
+				)
+				db.session.add(new_user)
+				db.session.commit()
 
-			flash('Accounted created.', category='success')
-			login_user(user, remember=True)
-			return redirect(url_for('views.home'))
+				flash('Accounted created.', category='success')
+				login_user(new_user, remember=True)
+				return redirect(url_for('views.home'))
 
 	return render_template("signup.html")
 
