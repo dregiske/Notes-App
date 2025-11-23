796fe605b86712a46bbb092064d2fd1487976a39
Andre Giske <agiske@ucsd.edu>
2025-11-22 18:08:35 -0800

    login manager

--- website/auth.py
+++ website/auth.py
@@ -5,6 +5,8 @@ from . import db
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
@@ -16,6 +18,10 @@ def signup():
 		firstName = request.form.get('firstName')
 		password1 = request.form.get('password1')
 		password2 = request.form.get('password2')
+
+		user = User.query.filter_by(email=email).first()
+		if user:
+			flash('Email already exists.', category='error')
 
 		if len(email) < 4:
 			flash('Email must be greater than 4 characters.', category='error')
@@ -35,14 +41,32 @@ def signup():
 			db.session.commit()
 
 			flash('Accounted created.', category='success')
+			login_user(user, remember=True)
 			return redirect(url_for('views.home'))
 
 	return render_template("signup.html")
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		email = request.form.get('email')
+		password = request.form.get('password')
+
+		user = User.query.filter_by(email=email).first()
+		if user:
+			if check_password_hash(user.password, password):
+				flash('Logged in successfully', category='success')
+				login_user(user, remember=True)
+				return redirect(url_for('views.home'))
+			else:
+				flash('Retry password or email.', category='error')
+		else:
+			flash('Email does not exist.', category='error')
+
 	return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-	return "<p>Logout</p>"
+	logout_user()
+	return redirect(url_for('auth.login'))
