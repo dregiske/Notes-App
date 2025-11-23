4eed3e730bbc462e5e37ec194170b43d598490e8
Andre Giske <agiske@ucsd.edu>
2025-11-22 20:59:06 -0800

    finishing touches

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .models import User
 from . import db
 
-from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
